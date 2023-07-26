bb8904b102d56ebf1fe8d657980d8ccdbc464390
Akanni Emmanuel <akannie33@gmail.com>
2023-07-26 15:30:45 -0700

    🧽 the syntax 🙆‍♂️

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([createUser(), uploadPhoto()]).then((results) => {
     const [userResult, photoResult] = results;
-    console.log(`${photoResult.body} ${userResult.firstname} ${userResult.lastname}`);
+    console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
     // eslint-disable-next-line no-unused-vars
   }).catch((error) => {
     console.log('Signup system offline');
