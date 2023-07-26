e451a18be893661d8f4adc222a2c45f48c882433
Akanni Emmanuel <akannie33@gmail.com>
2023-07-26 16:36:59 -0700

    ❌ forget to resolve to the expected structure

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => values);
+    .then((res) => (
+      res.map((val) => ({
+        status: val.status,
+        value: val.status === 'rejected' ? String(val.reason) : val.value,
+      }))
+    ));
 }
