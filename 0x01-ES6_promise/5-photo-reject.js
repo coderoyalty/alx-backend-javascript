83977a60e653eb437c3ddfcd8981cccacc80c1a7
Akanni Emmanuel <akannie33@gmail.com>
2023-07-26 15:47:25 -0700

    ❌ code issue fixed

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,5 @@
 export default function uploadPhoto(filename) {
   return new Promise((_, reject) => {
-    // eslint-disable-next-line prefer-promise-reject-errors
-    reject(`${filename} cannot be processed`);
+    reject(new Error(`${filename} cannot be processed`));
   });
 }
