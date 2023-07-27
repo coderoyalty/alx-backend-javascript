366bb0bbac6ce814334158f060ed080ecf4bbd64
Akanni Emmanuel <akannie33@gmail.com>
2023-07-27 12:18:53 -0700

    ✅ task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable class-methods-use-this */
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
