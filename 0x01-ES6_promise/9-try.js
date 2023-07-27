5aad748d06fc9e01973003a8d367f487ceba8f2b
Akanni Emmanuel <akannie33@gmail.com>
2023-07-26 17:25:51 -0700

    okay! I promise I'll fixed it

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,8 +4,10 @@ export default function guardrail(mathFunction) {
     const value = mathFunction();
     queue.push(value);
   } catch (error) {
-    queue.push(error.message);
+    queue.push(`Error: ${error.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
+
+  return queue;
 }
