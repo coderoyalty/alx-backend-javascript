adc95c2df4ee7de23db2bb45853fae154305b520
Akanni Emmanuel <akannie33@gmail.com>
2023-07-26 17:23:16 -0700

    😞 how could I foget finally?

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,10 +3,9 @@ export default function guardrail(mathFunction) {
   try {
     const value = mathFunction();
     queue.push(value);
-    queue.push('Guardrail was processed');
   } catch (error) {
     queue.push(error.message);
+  } finally {
     queue.push('Guardrail was processed');
   }
-  return queue;
 }
