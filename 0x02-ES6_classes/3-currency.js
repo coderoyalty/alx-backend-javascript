18bee623446913ce7ee529c46120a3c95670b589
Akanni Emmanuel <akannie33@gmail.com>
2023-07-27 11:45:49 -0700

    ✔ task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-underscore-dangle */
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+}
