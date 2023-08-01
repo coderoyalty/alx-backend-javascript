f7eaca927bd3d60957a96ea462b4e26a712ba1c9
Akanni Emmanuel <akannie33@gmail.com>
2023-08-01 11:56:03 -0700

    🧹 return a data view

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,9 +2,10 @@ export default function createInt8TypedArray(length, position, value) {
   try {
     const buffer = new ArrayBuffer(length);
     const int8Array = new Int8Array(buffer);
+    const dataView = new DataView(buffer);
     int8Array[position] = value;
 
-    return buffer;
+    return dataView;
   } catch (err) {
     throw Error('Position outside range');
   }
