48da14a4da06b353f6bc7f388fe854bce0b8f895
Akanni Emmanuel <akannie33@gmail.com>
2023-08-01 12:08:36 -0700

    🔧fixed solution

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-  try {
-    const buffer = new ArrayBuffer(length);
-    const int8Array = new Int8Array(buffer);
-    const dataView = new DataView(buffer);
-    int8Array[position] = value;
-
-    return dataView;
-  } catch (err) {
-    throw Error('Position outside range');
+  if (length <= position) {
+    throw new Error('Position outside range');
   }
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  const dataview = new DataView(buffer);
+  int8Array[position] = value;
+
+  return dataview;
 }
