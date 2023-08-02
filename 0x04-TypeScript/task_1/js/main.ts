14f873ef6049ac51fe59f7643ce34517a56d1c7d
Akanni Emmanuel <akannie33@gmail.com>
2023-08-02 16:07:28 -0700

    ✅ ts: task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,4 +17,30 @@ export interface printTeacherFunction {
 
 export function printTeacher(firstName: string, lastName: string): string {
   return `${lastName[0].toUpperCase()}. ${firstName}`;
-} 
+}
+
+export interface IStudent {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export interface IStudentConstructor {
+  new (firstName: string, lastName: string): IStudent;
+}
+
+export class StudentClass implements IStudent {
+  private firstName: string;
+  private lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
